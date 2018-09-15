6bce36bbe682dfc7ada6807f43f618fd7fc744ec
Daniel Storey <dstorey@spartaglobal.com>
2018-09-15 13:20:49 +0100

    Moved to class system, added 'this' to EVERYTHING, added get/set methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,76 +1,80 @@
 function buttonPress() {
   var element = document.getElementById("sven");
   element.innerHTML = "That";
-  initialiseDeck();
+  var playDeck = new Deck();
+  playDeck.initialise();
 }
 
-var card = {suit: "", value: 0}
-var suits = ["S","H","D","C"];
-var deck = [];
-
-function initialiseDeck() {
-  const SUITS_QUANTITY = 4;
-  const SUIT_SIZE = 13;
-
-  // Build a sorted deck in suits descending order - S,H,D,C
-  for(var i = 0; i < SUITSQUANTITY; i++) {
-    for(var j = 0; j < SUIT_SIZE; j++) {
-      card.suit = suits[i];
-      card.value = (j+1).toString();
-
-      //   // SWITCH STATEMENT THAT NEVER PRODUCES A KING...
-      //   // switch (j) {
-      //   //   case 10:
-      //   //     card.value = "J";
-      //   //     break;
-      //   //   case 11:
-      //   //     card.value = "Q";
-      //   //     console.log(j);
-      //   //     break;
-      //   //   case 12:
-      //   //     card.vaue = "K";
-      //   //     break;
-      //   //   default:
-      //   //     throw "invalidCardError";
-      //   // }
-      //
-      //   if(j === 10) {
-      //     card.value = "J";
-      //   }
-      //   else if(j === 11) {
-      //     card.value = "Q";
-      //   }
-      //   else if(j === 12) {
-      //     card.value = "K";
-      //   }
-      //   else {
-      //     throw "invalidCardError";
-      //   }
-
-      console.log("Card: " + card.suit + card.value);
-      deck.push(card);
-    }
-    console.log("Suit End");
+class Deck {
+  constructor() {
+    this.card = {suit: "", value: 0}
+    this.suits = ["S","H","D","C"];
+    this.deck = [];
   }
-  console.log("Deck size: " + deck.length);
-  console.log("Deck initialised");
-}
 
-// Could randomly grab a single card from the deck rather than suffle
-// BUT a shuffled deck allows for reusable code in other card games
-function shuffleDeck() {
-  // TODO: Shuffle the deck
-}
+  initialise() {
+    const SUITS_QUANTITY = 4;
+    const SUIT_SIZE = 13;
 
-function dealCard(player) {
-  // TODO: Give a single card to a designated player
-}
+    // Build a sorted deck in suits descending order - S,H,D,C
+    for(var i = 0; i < SUITS_QUANTITY; i++) {
+      for(var j = 0; j < SUIT_SIZE; j++) {
+        this.card.suit = this.suits[i];
+        this.card.value = (j+1).toString();
 
-//
-function deleteFromDeck(c) {
-  // TODO: Remove card from list of available cards
-}
+        //   // SWITCH STATEMENT THAT NEVER PRODUCES A KING...
+        //   // switch (j) {
+        //   //   case 10:
+        //   //     card.value = "J";
+        //   //     break;
+        //   //   case 11:
+        //   //     card.value = "Q";
+        //   //     console.log(j);
+        //   //     break;
+        //   //   case 12:
+        //   //     card.vaue = "K";
+        //   //     break;
+        //   //   default:
+        //   //     throw "invalidCardError";
+        //   // }
+        //
+        //   if(j === 10) {
+        //     card.value = "J";
+        //   }
+        //   else if(j === 11) {
+        //     card.value = "Q";
+        //   }
+        //   else if(j === 12) {
+        //     card.value = "K";
+        //   }
+        //   else {
+        //     throw "invalidCardError";
+        //   }
 
-function dealHands(players) {
-  // TODO: Deal out necessary number of cards to players (7 for 2/3 players, 5 for 4/5 players)
+        this.deck.push(this.card);
+      }
+    }
+    console.log("Deck initialised");
+    console.log("Deck size: " + this.deck.length);
+  }
+
+  // Could randomly grab a single card from the deck rather than suffle
+  // BUT a shuffled deck allows for reusable code in other card games
+  shuffleDeck() {
+    // TODO: Shuffle the deck
+  }
+
+  getCard() {
+    // TODO: Give a single card to a designated player
+  }
+
+  //
+  deleteFromDeck(c) {
+    // TODO: Remove card from list of available cards
+  }
+
+  // 'dealHands' will now be dealt with by 'Dealer' class
+  // dealHands(players) {
+  //   // TODO: Deal out necessary number of cards to players (7 for 2/3 players, 5 for 4/5 players)
+  // }
 }
